58d22595721338a01b62ec7bf878caf7146161d9
Pavan Kumar <pavan.kumar@mahoujas.com>
2015-04-28 18:37:36 +0530

    Version 1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'mahoujas:smsowl',
-    version: '1.0.0',
+    version: '1.0.1',
     summary: 'Wrapper for smsowl.in REST API.',
     git: 'https://github.com/mahoujas/smsowl-meteor.git',
     documentation: 'README.md'
